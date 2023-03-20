45b1c0839bf9b4e3a42bc49b959fde4c684cdb5a
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-03-20 23:41:24 +0200

    add button, add function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import { Component } from "react";
 // import API from "./Api/Api"
 
 import Searchbar from "./Searchbar/Searchbar";
-import ImageGallery from 'components/ImageGallery/ImageGallery';
+import ImageGallery from "components/ImageGallery/ImageGallery";
+import Button from "components/Button/Button";
 
 export default class App extends Component {
 
@@ -21,13 +22,30 @@ handleSubmitInput = newQuery => {
     if (newQuery !== this.state.name) {
       this.setState({ name: newQuery, page: 1, status: 'pending' });
     }
+};
+  
+handleClickImg = event => {
+    const imgForModal = event.target.dataset.src;
+    const altForModal = event.target.alt;
+    this.setState({
+      showModal: true,
+      modalImg: imgForModal,
+      modalAlt: altForModal,
+    });
+  };
+  
+  handleClickBtn = () => {
+    this.setState(({ page }) => {
+      return { page: page + 1, status: 'pending' };
+    });
   };
 
   render() {
     return (
       <>
         <Searchbar onSubmit={this.handleSubmitInput} />
-        <ImageGallery/>
+        <ImageGallery onClickImg={this.handleClickImg} query={this.state.query} />
+        <Button handleClickBtn={this.handleClickBtn} />
       </>
     )
   }
